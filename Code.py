1c7ad17ac1729085d31a40c766aed70f9955e944
gautamgaddam <gautammaddyson@gmail.com>
2020-03-12 15:04:46 +0530

    Data types

---
+++ Code.py
@@ -0,0 +1,28 @@
+print("damn it works")
+print("damn it works tooooooooooo")
+name = 'srinivas'
+print(name)
+print(name.upper())
+print (name[2:5])
+L1 =['ray', 'taz', 'ytr', 'yug', 'taz']
+print(L1)
+L1.append(6)
+print(L1)
+L1.insert(2, 'tad')
+print(L1)
+L1.reverse()
+print(L1)
+Courses = {1: 'Science', 2: 'Social', 2: 'Soal', 3: 'Math', 'fourth': 'Chemistry'}
+print(Courses)
+print(Courses[3])
+print(Courses.get('fourth'))
+Courses['fourth'] = 'language C'
+
+print(Courses)
+bag = (10, 30, 10, 10, 'pen', 'pencile', 'chocolate', 'pen')
+print(bag)
+print(bag.count('pen'))
+set ={'ram', 'raju', 'shtam', 'ram'}
+print(set)
+print(list(range(100, 300)))
+print(list(range(100)))
