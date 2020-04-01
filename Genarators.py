af0d024e8c4ed325c72e662c9d4372a30db08d35
Srinivas <srinivasgaddam423@gmail.com>
2020-04-01 18:28:42 +0530

    Genarators

---
+++ Genarators.py
@@ -0,0 +1,42 @@
+# Fibonacci Series using Genarator functions
+
+#Sinewave
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sb
+def s(flip =2):
+    x = np.linspace(0, 14, 100)
+    for i in range(1, 5):
+        plt.plot(x, np.sin(x + i * .5) * (7 - i) * flip)
+sb.set()
+s()
+plt.show()
+print(next(s))
+
+def fib( ):
+    f, s=0, 1
+    while True:
+        yield f
+        f, s=s, f+s
+for x in fib():
+    if x>50:
+        break
+    print(x, end=" ")
+
+
+# Number Steam
+
+a= range(2, 100, 2)
+b =(x for x in a)
+print(b)
+for y in b:
+    print(y)
+
+a= range(100)
+b =(x for x in a)
+print(b)
+for y in b:
+    print(y)
+
