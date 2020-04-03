53c726e47788cfc04ef8c83f860c52ffed1ad4fa
Srinivas <srinivasgaddam423@gmail.com>
2020-04-03 14:54:48 +0530

    Genarators

--- Genarators.py
+++ Genarators.py
@@ -15,27 +15,19 @@ s()
 plt.show()
 print(next(s))
 
-def fib( ):
-    f, s=0, 1
-    while True:
-        yield f
-        f, s=s, f+s
-for x in fib():
-    if x>50:
-        break
-    print(x, end=" ")
+
 
 
 # Number Steam
 
-a= range(2, 100, 2)
-b =(x for x in a)
+a = range(2, 100, 2)
+b = (x for x in a)
 print(b)
 for y in b:
     print(y)
 
-a= range(100)
-b =(x for x in a)
+a = range(100)
+b = (x for x in a)
 print(b)
 for y in b:
     print(y)
