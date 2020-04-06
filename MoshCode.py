00425ea9e72030416d7d9fe4ecb7fcd2aa9dcb0c
Srinivas <srinivasgaddam423@gmail.com>
2020-04-06 18:30:29 +0530

    MoshCode Basics

---
+++ MoshCode.py
@@ -0,0 +1,52 @@
+# BackSlash \ is the escape Charactor
+
+course = "   PYTHON \"course"
+print(course)
+
+#formatted Strings
+
+first ="Srinivas"
+Last = "Gaddam"
+full = f"{first} {Last}"
+print(full)
+
+#Function Available on string
+
+print(course.upper())
+print(course.strip())
+#find the index
+print(course.find("cou"))
+#replace the string
+
+print(course.replace("P", "z"))
+
+# Complex Numbers
+
+x = 1 + 2j
+
+
+y = 6 + 7j
+
+z = x / y
+
+print(z)
+
+# rounding a number
+
+print(round(2.9))
+
+# abs of number
+
+print(abs(-2.9))
+
+# input value manually
+
+x = input("x: ")
+
+
+
+y = int(x) +1
+
+
+print(f"x: {x}, y: {y}")
+
