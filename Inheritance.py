1cf13136955efbf90632a028c5a34cc0092bee44
Srinivas <srinivasgaddam423@gmail.com>
2020-04-28 19:52:34 +0530

    inheritance

--- Inheritance.py
+++ Inheritance.py
@@ -85,9 +85,10 @@ list.append("1")
 
 
 class Point:
-    def __init__(self, x ,y):
+    def __init__(self, x, y):
         self.x =x
         self.y =y
+
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 p1 = Point(1,2)
