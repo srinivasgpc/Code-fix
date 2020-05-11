2394cd53fdb0047ce16a09a820bf140faf83d2b3
Srinivas <srinivasgaddam423@gmail.com>
2020-05-11 13:24:59 +0530

    create operators in django and display dynamically

--- Django/App/mysite/calc/views.py
+++ Django/App/mysite/calc/views.py
@@ -4,3 +4,16 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html',{'name':'srinivas'})
+
+
+
+
+def operator(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    Add = val1 + val2
+    sub = val1 - val2
+    mul = val1 * val2
+    div = val1 / val2
+    avg = (val1 +val2) //2
+    return render(request, 'result.html', {'Addition': Add , 'Subtraction': sub, 'multiply': mul, 'divide' : div, 'avarage' : avg })
