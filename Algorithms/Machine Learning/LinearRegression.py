47084cd2249cb3693f2767273a0a80a7c2daea5b
Srinivas <srinivasgaddam423@gmail.com>
2020-06-01 11:43:12 +0530

    Machine learning algorithms

---
+++ Algorithms/Machine Learning/LinearRegression.py
@@ -0,0 +1,14 @@
+"""
+        Linear Regression
+
+    It is a statistic model used to predict the relationship between independent and dependent variable
+
+    Application:
+    1. Predict the crop yield
+    2. Growth rate for a Country with predicting the GDP
+    3. Product market Price
+    4. House sales like number of houses can be sold my a builder
+    5. Score prediction in sports
+
+
+"""
